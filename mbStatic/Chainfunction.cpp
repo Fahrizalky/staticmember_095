06ef250f168ceb73d2e513e4ec5206d440f68d3f
Fahrizalky <fahrizalkusumayuda2003@gmail.com>
2023-05-17 12:22:48 +0700

    menggunakan perintah this dan gejudul

--- mbStatic/Chainfunction.cpp
+++ mbStatic/Chainfunction.cpp
@@ -6,4 +6,12 @@ class buku {
 public:
 	buku setJudul(string judul)
 	{
-		
+		this->judul = judul;
+		return *this; //chain function
+	}
+	string getJudul()
+	{
+		return this->judul;
+	}
+} bukunya;
+
