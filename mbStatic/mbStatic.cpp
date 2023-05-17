fcc7f8b10defa4073c64562074099661a3ee2bbe
Fahrizalky <fahrizalkusumayuda2003@gmail.com>
2023-05-17 11:07:34 +0700

    menjalankan fungsi void dari setID dan print all

--- mbStatic/mbStatic.cpp
+++ mbStatic/mbStatic.cpp
@@ -13,5 +13,19 @@ public:
 	mahasiswa(string pnama) : nama(pnama) { setID(); }
 };
 
+int mahasiswa::nim = 0;
+
+void mahasiswa::setID() {
+	id = ++nim;
+}
+
+void mahasiswa::printAll() {
+	cout << "ID =" << id << endl;
+	cout << "Nama =" << nama << endl;
+	cout << endl;
+}
+
+
+
 
 
